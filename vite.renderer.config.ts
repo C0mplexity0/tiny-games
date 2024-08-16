af55c5006193472737a0e6fcef1dd37dd5126213
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-08-16 22:17:29 +0100

    fix: Probably should have tested that last change

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,6 +1,7 @@
 import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
 import { pluginExposeRenderer } from "./vite.base.config";
+import path from "path";
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -15,9 +16,18 @@ export default defineConfig((env) => {
     build: {
       outDir: `.vite/renderer/${name}`,
     },
-    plugins: [pluginExposeRenderer(name)],
+    plugins: [
+      pluginExposeRenderer(name)
+    ],
     resolve: {
       preserveSymlinks: true,
+      alias: {
+        "@app": path.resolve(__dirname, "src/ui/app"),
+        "@web": path.resolve(__dirname, "src/ui/web"),
+        "@lib": path.resolve(__dirname, "src/ui/lib"),
+        "@styles": path.resolve(__dirname, "src/ui/styles"),
+        "@components": path.resolve(__dirname, "src/ui/components"),
+      },
     },
     optimizeDeps: {
       exclude: ["js-big-decimal"]
