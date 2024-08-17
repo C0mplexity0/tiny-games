7b5ced2d84bfc12c2906d16d4199283e1c746358
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-08-17 17:14:42 +0100

    fix: Okay maybe importing React was actually important

--- src/ui/app/App.tsx
+++ src/ui/app/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { HashRouter, Outlet, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home";
 
--- src/ui/app/index.tsx
+++ src/ui/app/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import React from "react";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
