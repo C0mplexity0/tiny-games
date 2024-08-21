97f55d7d9e00614642ea471b2815af0e5edf2440
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-08-21 14:40:43 +0100

    refactor: Move device-button component to a new file

--- src/ui/app/pages/home.tsx
+++ src/ui/app/pages/home.tsx
@@ -1,68 +1,29 @@
-import { Device } from "@/devices/devices";
-import { devices } from "@app/App";
+import { Game } from "@/games/games";
+import { devices, games } from "@app/App";
 import { Button } from "@components/ui/button";
-import DeviceNewButton from "@components/ui/devices/new";
-import DeviceProfile from "@components/ui/devices/profile";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@components/ui/dropdown-menu";
-import { Popover, PopoverContent, PopoverTrigger } from "@components/ui/popover";
-import { EllipsisVertical } from "lucide-react";
-import React from "react";
+import DeviceButton from "@components/ui/devices/device-button";
+import BrowserLink from "@components/util/browser-link";
+import { SiFacebook, SiGithub, SiPatreon, SiReddit, SiReplit, SiTumblr, SiX, SiYoutube } from "@icons-pack/react-simple-icons";
+import { Gamepad2, Link, Play } from "lucide-react";
+import React, { ReactNode, useEffect, useState } from "react";
 
-function DeviceProfileOptions({ device }: { device: Device }) {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size="icon" variant="outline">
-          <EllipsisVertical />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem 
-          color="red"
-          onClick={() => {
-            window.electron.ipcRenderer.sendMessage("removeDevice", device.id);
-          }}
-        >
-          Remove
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+
+let currentGame: Game;
+let setCurrentGame: (currentGame: Game) => void;
+
+
+const SOCIAL_ICONS: { [social: string]: ReactNode } = {
+  "github.com": <SiGithub size="16px" />,
+  "youtube.com": <SiYoutube size="16px" />,
+  "twitter.com": <SiX size="16px" />,
+  "x.com": <SiX size="16px" />,
+  "replit.com": <SiReplit size="16px" />,
+  "facebook.com": <SiFacebook size="16px" />,
+  "tumblr.com": <SiTumblr size="16px" />,
+  "reddit.com": <SiReddit size="16px" />,
+  "patreon.com": <SiPatreon size="16px" />
 }
 
-function DeviceButton({ device, newButton }: { device?: Device, newButton?: Boolean, isMainDevice?: Boolean }) {
-  return (
-    <div className="size-12">
-      {
-        newButton ?
-        <DeviceNewButton to="/devices/add-devices" />
-        :
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button size="icon" className={"size-12 rounded-[37.5%] bg-accent hover:bg-accent/90 relative " + (device.connected ? "" : "bg-secondary hover:bg-secondary/90")}>
-              <DeviceProfile className="bg-transparent" device={device} />
-            </Button>
-          </PopoverTrigger>
-
-          <PopoverContent side="right">
-            <div className="flex flex-col gap-2">
-
-              <div className="w-full flex flex-row">
-                <span className="flex-1 overflow-hidden whitespace-nowrap text-xl leading-10 align-middle text-ellipsis">{device.username}</span>
-                <DeviceProfileOptions device={device} />
-              </div>
-
-              <div className="w-full flex flex-row gap-1 text-secondary-foreground">
-                <span>{device.connected ? `Connected, Ping: ${device.latency}ms` : "Reconnecting..."}</span>
-              </div>
-              
-            </div>
-          </PopoverContent>
-        </Popover>
-      }
-    </div>
-  )
-}
 
 function Sidebar() {
   return (
@@ -81,10 +42,143 @@ function Sidebar() {
   )
 }
 
+function GameButton({ game }: { game: Game }) {
+  return (
+    <Button 
+      onClick={() => {
+        setCurrentGame(game);
+      }}
+      className="bg-secondary hover:bg-secondary/90 text-secondary-foreground w-full justify-start h-12 p-2 flex flex-row space-x-3"
+    >
+      {
+        game.icon ?
+        <img src={game.icon} className="size-8 rounded-sm border" /> :
+        <div className="size-8 rounded-sm border relative"><Gamepad2 className="size-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" /></div>
+      }
+      <h2 className="font-bold inline-block h-8 leading-8 align-middle">{game.name}</h2>
+    </Button>
+  )
+}
+
+function GamesList() {
+  return (
+    <div className="p-2 w-56 md:w-72 lg:w-96 h-full border-r">
+      {
+        games.map((game, i) => <GameButton game={game} key={i} />)
+      }
+    </div>
+  )
+}
+
+function getSocialIcon(social: string) {
+  const url = new URL(social);
+  const icon = SOCIAL_ICONS[url.hostname];
+
+  if (icon) {
+    return icon;
+  }
+
+  return <Link size="16px" />
+}
+
+function SocialButton({ social }: { social: string }) {
+  return (
+    <Button size="icon" variant="ghost" className="size-9" asChild>
+      <BrowserLink to={social}>
+        {
+          getSocialIcon(social)
+        }
+      </BrowserLink>
+    </Button>
+  )
+}
+
+function GamePage({ game }: { game: Game }) {
+  if (!game) {
+    return (
+      <div></div>
+    )
+  }
+
+  return (
+    <div className="size-full relative">
+      <div 
+        className="w-full h-[50vh] bg-center bg-no-repeat bg-cover relative"
+        style={{
+          backgroundImage: `url(${game.thumbnail})`
+        }}
+      >
+        <div 
+          className="w-full h-full absolute bottom-0" 
+          style={{
+            backgroundImage: "linear-gradient(180deg, hsl(var(--background) / 0%) 0%, hsl(var(--background) / 100%) 90%)"
+          }}
+        />
+      </div>
+
+      <div className="absolute w-full h-fit p-4 left-0 top-[50vh] -translate-y-24 flex flex-col gap-y-4">
+        <div className="flex flex-row gap-x-2 mb-4">
+          <img src={game.icon} className="rounded-2xl size-32 shadow-lg shadow-stone-900 border" />
+          <div>
+            <div className="h-20">
+              <h2 className="text-3xl font-bold m-0 p-0">{game.name}</h2>
+              {
+                game.author ?
+                <span className="m-0 p-0 text-secondary-foreground">By {game.author}</span> :
+                null
+              }
+            </div>
+            <Button 
+              className="w-44 h-10"
+              onClick={() => {
+                window.electron.ipcRenderer.sendMessage("playGame", games.indexOf(game));
+              }}
+            ><Play className="mr-2 h-4 w-4" />Play</Button>
+          </div>
+        </div>
+        
+        {
+          game.description ?
+          <span className="text-secondary-foreground w-full overflow-hidden text-ellipsis">{game.description}</span> :
+          null
+        }
+
+        {
+          game.socials ?
+          <div className="rounded-md p-0 flex flex-row w-fit">
+            {
+              game.socials.map((social, i) => <SocialButton social={social} key={i} />)
+            }
+          </div> :
+          null
+        }
+      </div>
+    </div>
+  )
+}
+
+
 export default function AppHomePage() {
+  [currentGame, setCurrentGame] = useState();
+
+  useEffect(() => {
+    if (games) {
+      setCurrentGame(games[0]);
+    }
+  }, [games]);
+
   return (
     <div className="size-full flex flex-row">
       <Sidebar />
+
+      <div className="flex-1 flex flex-row">
+        <GamesList />
+        <div className="flex-1">
+          {
+            <GamePage game={currentGame} />
+          }
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/ui/components/ui/devices/device-button.tsx
@@ -0,0 +1,64 @@
+import { Device } from "@/devices/devices";
+import { EllipsisVertical } from "lucide-react";
+import React from "react";
+import { Button } from "../button";
+import DeviceNewButton from "./new";
+import DeviceProfile from "./profile";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../dropdown-menu";
+import { Popover, PopoverContent, PopoverTrigger } from "../popover";
+
+function DeviceProfileOptions({ device }: { device: Device }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="outline">
+          <EllipsisVertical />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem 
+          color="red"
+          onClick={() => {
+            window.electron.ipcRenderer.sendMessage("removeDevice", device.id);
+          }}
+        >
+          Remove
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default function DeviceButton({ device, newButton, popoverSide }: { device?: Device, newButton?: Boolean, popoverSide?: "top" | "bottom" | "left" | "right" }) {
+  return (
+    <div className="size-12">
+      {
+        newButton ?
+        <DeviceNewButton to="/devices/add-devices" />
+        :
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="icon" className={"size-12 rounded-[37.5%] bg-accent hover:bg-accent/90 relative " + (device.connected ? "" : "bg-secondary hover:bg-secondary/90")}>
+              <DeviceProfile className="bg-transparent" device={device} />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent side={popoverSide ? popoverSide : "right"}>
+            <div className="flex flex-col gap-2">
+
+              <div className="w-full flex flex-row">
+                <span className="flex-1 overflow-hidden whitespace-nowrap text-xl leading-10 align-middle text-ellipsis">{device.username}</span>
+                <DeviceProfileOptions device={device} />
+              </div>
+
+              <div className="w-full flex flex-row gap-1 text-secondary-foreground">
+                <span>{device.connected ? `Connected, Ping: ${device.latency}ms` : "Reconnecting..."}</span>
+              </div>
+              
+            </div>
+          </PopoverContent>
+        </Popover>
+      }
+    </div>
+  )
+}
