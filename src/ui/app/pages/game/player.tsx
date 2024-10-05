85a307e8a90af09602ba0e9a41a11e74d6513240
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-10-05 18:49:42 +0100

    fix: URL is randomly logged when a game starts

--- src/ui/app/pages/game/player.tsx
+++ src/ui/app/pages/game/player.tsx
@@ -111,8 +111,6 @@ export default function PlayerPage() {
               urlStr = "*";
             }
 
-            console.log(urlStr);
-
             postMessage("setParentUrl", urlStr);
           }}
         />
