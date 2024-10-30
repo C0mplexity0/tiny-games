2810402db18cc6ae0d6f91cecfec2cf91b315587
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2024-10-30 16:24:23 +0000

    feat: Dropdown menu "destructive" prop

--- src/ui/components/ui/devices/device-button.tsx
+++ src/ui/components/ui/devices/device-button.tsx
@@ -17,7 +17,7 @@ export function DeviceProfileOptions({ device }: { device: Device }) {
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem 
-          color="red"
+          destructive
           onClick={() => {
             window.electron.ipcRenderer.sendMessage("removeDevice", device.id);
           }}
--- src/ui/components/ui/dropdown-menu.tsx
+++ src/ui/components/ui/dropdown-menu.tsx
@@ -77,13 +77,15 @@ DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
 const DropdownMenuItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
-    inset?: boolean
+    inset?: boolean,
+    destructive?: boolean
   }
->(({ className, inset, ...props }, ref) => (
+>(({ className, inset, destructive, ...props }, ref) => (
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      destructive ? "focus:bg-destructive" : "",
       inset && "pl-8",
       className
     )}
